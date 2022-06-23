21a87c2343b3f534dc6f529cf37e29fc199fe65d
Sallie McKee <salliemckee7@gmail.com>
2022-06-23 13:30:15 -0500

    added server side props and finished tutorial

--- pages/cars/[id].js
+++ pages/cars/[id].js
@@ -1,8 +1,52 @@
 import { useRouter } from "next/router";
 
-export default function Car() {
+import Head from "next/head";
+
+export default function Car({ car }) {
   const router = useRouter();
   const { id } = router.query;
 
-  return <h1>Hello {id}</h1>;
+  return (
+    <>
+      <Head>
+        <title>
+          {car.color} {car.id}
+        </title>
+      </Head>
+      <h1>Hello {id}</h1>
+      <img src={car.image} alt="car picture" width="300px" />
+    </>
+  );
 }
+
+export async function getServerSideProps({ params }) {
+  const req = await fetch(`http://localhost:3000/${params.id}.json`);
+  const data = await req.json();
+
+  return {
+    props: { car: data },
+  };
+}
+
+// export async function getStaticProps({ params }) {
+//   const req = await fetch(`http://localhost:3000/${params.id}.json`);
+//   const data = await req.json();
+
+//   return {
+//     props: { car: data },
+//   };
+// }
+
+// export async function getStaticPaths() {
+//   const req = await fetch(`http://localhost:3000/cars.json`);
+//   const data = await req.json();
+
+//   const paths = data.map((car) => {
+//     return { params: { id: car } };
+//   });
+
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
